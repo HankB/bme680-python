c76aea58de55c1e8c7023675a1a5162275b32257
Hank Barta <hbarta@gmail.com>
2023-06-05 10:49:23 -0500

    strip out unneeded output, prime sensor

--- examples/json-all.py
+++ examples/json-all.py
@@ -3,28 +3,11 @@
 import bme680
 import time
 
-print("""read-all.py - Displays temperature, pressure, humidity, and gas.
-
-Press Ctrl+C to exit!
-
-""")
 
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 except (RuntimeError, IOError):
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
-
-# These calibration data can safely be commented
-# out, if desired.
-
-print('Calibration data:')
-for name in dir(sensor.calibration_data):
-
-    if not name.startswith('_'):
-        value = getattr(sensor.calibration_data, name)
-
-        if isinstance(value, int):
-            print('{}: {}'.format(name, value))
 
 # These oversampling settings can be tweaked to
 # change the balance between accuracy and noise in
@@ -36,13 +19,6 @@ sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
 
-print('\n\nInitial reading:')
-for name in dir(sensor.data):
-    value = getattr(sensor.data, name)
-
-    if not name.startswith('_'):
-        print('{}: {}'.format(name, value))
-
 sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(150)
 sensor.select_gas_heater_profile(0)
@@ -51,25 +27,23 @@ sensor.select_gas_heater_profile(0)
 # with their own temperature and duration.
 # sensor.set_gas_heater_profile(200, 150, nb_profile=1)
 # sensor.select_gas_heater_profile(1)
+sensor.get_sensor_data() # prime sensor
+time.sleep(0.1)
+sensor.get_sensor_data() # prime sensor
+time.sleep(0.1)
+if sensor.get_sensor_data():
+    output = '{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH'.format(
+        sensor.data.temperature,
+        sensor.data.pressure,
+        sensor.data.humidity)
 
-print('\n\nPolling:')
-try:
-    while True:
-        if sensor.get_sensor_data():
-            output = '{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH'.format(
-                sensor.data.temperature,
-                sensor.data.pressure,
-                sensor.data.humidity)
+    if sensor.data.heat_stable:
+        print('{0},{1} Ohms'.format(
+            output,
+            sensor.data.gas_resistance))
 
-            if sensor.data.heat_stable:
-                print('{0},{1} Ohms'.format(
-                    output,
-                    sensor.data.gas_resistance))
+    else:
+        print(output)
+else:
+    print("sensor.get_sensor_data() unsuccessful")
 
-            else:
-                print(output)
-
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    pass
